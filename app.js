48c97d75ee3b6e3105475faf06089029a0c36ea1
clostao <hastro1990@gmail.com>
2021-07-30 19:55:34 +0200

    ADD: Bin folder

--- app.js
+++ app.js
@@ -1,59 +1,87 @@
-var fs = require('fs');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const {format} = require('util');
+const express = require('express');
+const Multer = require('multer');
 
-var app = express();
+// By default, the client will authenticate using the service account file
+// specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
+// the project specified by the GOOGLE_CLOUD_PROJECT environment variable. See
+// https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/docs/authentication.md
+// These environment variables are set automatically on Google App Engine
+const {Storage} = require('@google-cloud/storage');
 
-app.use(logger('dev'));
+// Instantiate a storage client
+const storage = new Storage();
+
+const app = express();
+app.set('view engine', 'pug');
+
+// This middleware is available in Express v4.16.0 onwards
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/files/*', async function(req, res, next) {
-  let filepath = req.path.replace("/files/", "").split("?")[0];
-  filepath = path.join('./files/', filepath);
-  let exists = await fs.promises.stat(filepath).then(() => true).catch(() => false);
-  if (exists) {
-    let stream = fs.createReadStream(filepath);
-    stream.pipe(res);
-    res.on('end', () => res.sendStatus(200));
-    return;
-  }
-  res.sendStatus(404);
-})
-
-app.post('/files/*', async function(req, res, next) {
-  let filepath = req.path.replace("/files/", "").split("?")[0];
-  let folderPath = path.join('./files/', filepath.split('/').slice(0, -1).join('/'));
-  await fs.promises.mkdir(folderPath, { recursive: true }).catch(e => e);
-  filepath = path.join('./files/', filepath);
-  let exists = await fs.promises.stat(filepath).then(e => true).catch(err => false);
-  if (exists) {
-    console.log("File already exists");
-    return res.sendStatus(409);
-  }
-  let stream = fs.createWriteStream(filepath);
-  req.pipe(stream);
-  req.on('error', () => res.sendStatus(400));
-  stream.on('error', () => res.sendStatus(500));
-  req.on('end', (chnk) => {
-    stream.end(chnk);
-    res.sendStatus(200);
-  });
+// Multer is required to process file uploads and make them available via
+// req.files.
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+  },
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// A bucket is a container for objects (files).
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET || "molten-reserve-321412.appspot.com");
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/files/*', async (req, res, next) => {
+  try {
+    let filepath = req.path.slice(1);
+    const blob = bucket.file(filepath);
+
+    if ((await blob.exists())[0]) {
+      const stream = blob.createReadStream();
+      stream.pipe(res);
+      res.on('error', () => res.sendStatus(500));
+      res.on('end', () => res.sendStatus(200));
+    } else {
+      res.sendStatus(404);
+    }
+
+  } catch (e) {
+    res.sendStatus(500).end(e);
+  }
+});
+
+
+// Process the file upload and upload to Google Cloud Storage.
+app.post('/files/*', async (req, res, next) => {
+  try {
+    let filepath = req.path.slice(1);
+    console.log(filepath);
+
+    // Create a new blob in the bucket and upload the file data.
+    const blob = bucket.file(filepath);
+    if ((await blob.exists())[0]) {
+      res.sendStatus(409);
+      return;
+    }
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+    });
+
+    req.pipe(blobStream);
+
+    blobStream.on('error', err => {
+      next(err);
+    });
+
+    blobStream.on('finish', () => res.sendStatus(200));
+  } catch (e) {
+    res.sendStatus(500).end(e);
+  }
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
 
 module.exports = app;
