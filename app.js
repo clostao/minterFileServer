7482d17ca54a4d35bf5761df90f52b44a719d347
clostao <hastro1990@gmail.com>
2021-07-30 14:11:52 +0200

    ADD: Added GET and POST to route /files/*

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-var createError = require('http-errors');
+var fs = require('fs');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var filesRouter = require('./routes/files');
-var uploadRouter = require('./routes/upload');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,13 +12,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/files', filesRouter);
+app.get('/files/*', async function(req, res, next) {
+  let filepath = req.path.replace("/files/", "").split("?")[0];
+  filepath = path.join('./files/', filepath);
+  let exists = await fs.promises.stat(filepath).then(() => true).catch(() => false);
+  if (exists) {
+    let stream = fs.createReadStream(filepath);
+    stream.pipe(res);
+    res.on('end', () => res.sendStatus(200));
+    return;
+  }
+  res.sendStatus(404);
+})
 
-app.use('/upload', uploadRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.post('/files/*', async function(req, res, next) {
+  let filepath = req.path.replace("/files/", "").split("?")[0];
+  let folderPath = path.join('./files/', filepath.split('/').slice(0, -1).join('/'));
+  await fs.promises.mkdir(folderPath, { recursive: true }).catch(e => e);
+  filepath = path.join('./files/', filepath);
+  let exists = await fs.promises.stat(filepath).then(e => true).catch(err => false);
+  if (exists) {
+    console.log("File already exists");
+    return res.sendStatus(409);
+  }
+  let stream = fs.createWriteStream(filepath);
+  req.pipe(stream);
+  req.on('error', () => res.sendStatus(400));
+  stream.on('error', () => res.sendStatus(500));
+  req.on('end', (chnk) => {
+    stream.end(chnk);
+    res.sendStatus(200);
+  });
 });
 
 // error handler
--- routes/files.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/files', function(req, res, next) {
-  res.contentType('app')
-});
-
-module.exports = router;
